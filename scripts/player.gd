1080d912fbf887d08b0c08b584290f7dc79688f7
sphinney <sphin@mit.edu>
2026-08-28 16:31:32 -0400

    change

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,6 +6,6 @@ extends XROrigin3D
 func _ready() -> void:
 	pass
 
-#func _process(_delta: float) -> void:
-	#Global.left_position = to_global(left_controller.position)
-	#Global.right_position = to_global(right_controller.position)
+func _process(_delta: float) -> void:
+	Global.left_position = to_global(left_controller.position)
+	Global.right_position = to_global(right_controller.position)
