d21f1de7f9461e04a164a0aa6432f0a0e8f12bc2
sphinney <sphin@mit.edu>
2026-08-28 13:40:56 -0400

    level 1 setup

--- scripts/main_menu_ui.gd
+++ scripts/main_menu_ui.gd
@@ -1,8 +1,16 @@
 extends Control
 
+@export var target: Node3D
+@export var offset_3d: Vector3
+
+func _process(_delta):
+	var pos_3d := target.global_position + offset_3d
+	var cam := get_viewport().get_camera_3d()
+	var pos_2d := cam.unproject_position(pos_3d)
+	global_position = pos_2d
+	visible = not cam.is_position_behind(pos_3d)
+
 func _ready() -> void:
-	pass
-func _process(_delta: float) -> void:
 	pass
 
 func _on_start_button_pressed() -> void:
