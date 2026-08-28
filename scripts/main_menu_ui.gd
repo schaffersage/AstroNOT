c82718d4ab6d2f0da947780270796584271ef516
sphinney <sphin@mit.edu>
2026-08-28 11:40:41 -0400

    tools?
    tools1

--- scripts/main_menu_ui.gd
+++ scripts/main_menu_ui.gd
@@ -6,7 +6,7 @@ func _process(_delta: float) -> void:
 	pass
 
 func _on_start_button_pressed() -> void:
-	pass # move to first task get_tree().change_scene_to_file('')
+	get_tree().change_scene_to_file('res://scenes/level_1.tscn')
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
